58e1d9b200561a03cfe3911ee7dc4d6983b3e2ea
Michał N <mnowinski@outlook.com>
2018-09-23 21:10:33 +0200

    added colored message in console

--- src/bot/ayasimple/ReadyListener.java
+++ src/bot/ayasimple/ReadyListener.java
@@ -10,13 +10,17 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
  */
 public class ReadyListener extends ListenerAdapter {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+
     public void onReady(ReadyEvent event){
         String out = "\nThis bot is running on the following servers: \n";
 
         for(Guild guild : event.getJDA().getGuilds()){
             out += guild.getName() + " (" + guild.getId() + ") \n";
         }
-        System.out.println(out);
+        System.out.println(ANSI_GREEN + out + ANSI_GREEN);
     }
 
 }
