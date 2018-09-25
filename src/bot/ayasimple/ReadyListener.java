5aeb9e992c5f5955674a90389413ea4f3a0fceb8
Michał N <mnowinski@outlook.com>
2018-09-25 21:14:33 +0200

    refactor cod

--- src/bot/ayasimple/ReadyListener.java
+++ src/bot/ayasimple/ReadyListener.java
@@ -4,6 +4,9 @@ import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author michlu
  * @sience 14.09.2018
@@ -15,12 +18,13 @@ public class ReadyListener extends ListenerAdapter {
     public static final String ANSI_GREEN = "\u001B[32m";
 
     public void onReady(ReadyEvent event){
-        String out = "\nThis bot is running on the following servers: \n";
+        String out = System.lineSeparator() + LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + " [AyaSimpleBOT] This bot is running on the following servers: \n";
 
         for(Guild guild : event.getJDA().getGuilds()){
-            out += guild.getName() + " (" + guild.getId() + ") \n";
+            out += guild.getName() + " (" + guild.getId() + ")" + System.lineSeparator();
         }
         System.out.println(ANSI_GREEN + out + ANSI_GREEN);
+        System.out.println(ANSI_RESET);
     }
 
 }
